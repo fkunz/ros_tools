89dda8623ab93b3c33c605c9e8722ab2f166d0b3
Florian Kunz <florian@fkunz.de>
2015-11-23 22:44:43 +0100

    Multithreaded input callback and publishing loop connected by a queue buffer. Checked with valgrind.

--- header_manipulation/include/header_manipulation/TimeModification.h
+++ header_manipulation/include/header_manipulation/TimeModification.h
@@ -17,7 +17,7 @@
 
 class TimeModification {
 public:
-    TimeModification(const ros::NodeHandle &nh, ros::Rate &publish_rate);
+    TimeModification(ros::Rate &publish_rate);
     ~TimeModification(){}
 
     void processShapeShifter(const topic_tools::ShapeShifter::Ptr shape_shifter);
@@ -27,29 +27,28 @@ public:
 private:
     typedef header_manipulation::TimeModificationConfig Config;
     typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
-    typedef std::pair<uint8_t[100000], ros::Time> StampedMsg;
-    typedef std::queue<StampedMsg> StampedMsgBuffer;
+    typedef std::pair<topic_tools::ShapeShifter, ros::Time> StampedMsg;
+    typedef std::queue<boost::shared_ptr<StampedMsg> > StampedMsgBuffer;
 
     void configCB(Config &config, uint32_t level);
     void inputCB(const topic_tools::ShapeShifter::ConstPtr &input);
-    void processBuffer(uint8_t *msg_buffer);
-    void publishMsg(const StampedMsg &stamped_msg);
-    void publishMsg(const uint8_t &msg_buffer, const ros::Time &time_to_pub);
+    void manipulateRawData(uint8_t *const msg_buffer);
+    void publishMsg(const boost::shared_ptr<StampedMsg> stamped_msg);
+    void publishMsg(const topic_tools::ShapeShifter &msg, const ros::Time &time_to_pub);
     void publishMsgLoop(const ros::NodeHandle &nh);
 
-    boost::thread msg_publisher_thread_;
     ros::NodeHandle private_nh_;
     ros::Subscriber generic_sub_;
     boost::shared_ptr<ReconfigureServer> reconf_server_;
-    unsigned int seq_counter_;
     bool output_advertised_;
+    unsigned int seq_counter_;
     ros::Publisher generic_pub_;
     boost::mutex buffer_mutex_, config_mutex_, pub_mutex_;
+    boost::thread msg_publisher_thread_;
     StampedMsgBuffer stamped_msg_buffer_;
 
-    double msg_delay_milliseconds_, time_offset_milliseconds_;
     ros::Duration msg_delay_, time_offset_;
-    ros::Rate publish_retry_rate_;
+    ros::Rate publish_rate_;
 };
 
 #endif //TIME_MODIFICATION_H
--- header_manipulation/src/TimeModification.cpp
+++ header_manipulation/src/TimeModification.cpp
@@ -1,17 +1,17 @@
 #include <header_manipulation/TimeModification.h>
 
-TimeModification::TimeModification(const ros::NodeHandle &nh, ros::Rate &publish_rate) :
-    publish_retry_rate_(publish_rate)
+TimeModification::TimeModification(ros::Rate &publish_rate) :
+    publish_rate_(publish_rate)
 {
-  private_nh_ = ros::NodeHandle("~");
-  generic_sub_ = private_nh_.subscribe<topic_tools::ShapeShifter>("input", 10,
-                                                                  &TimeModification::inputCB, this);
+    private_nh_ = ros::NodeHandle("~");
+    generic_sub_ = private_nh_.subscribe<topic_tools::ShapeShifter>("input", 10,
+                                                                    &TimeModification::inputCB, this);
 
-  reconf_server_.reset(new ReconfigureServer(private_nh_));
-  reconf_server_->setCallback(boost::bind(&TimeModification::configCB, this, _1, _2));
+    reconf_server_.reset(new ReconfigureServer(private_nh_));
+    reconf_server_->setCallback(boost::bind(&TimeModification::configCB, this, _1, _2));
 
-  seq_counter_ = 0;
-  output_advertised_ = false;
+    output_advertised_ = false;
+    seq_counter_ = 0;
 }
 
 void TimeModification::processShapeShifter(const topic_tools::ShapeShifter::Ptr shape_shifter)
@@ -23,30 +23,10 @@ void TimeModification::processShapeShifter(const topic_tools::ShapeShifter::Ptr 
 void TimeModification::processShapeShifter(topic_tools::ShapeShifter &shape_shifter)
 {
     ROS_DEBUG("TimeModification processShapeShifter");
-    std_msgs::Header header;
     uint8_t msg_buffer[shape_shifter.size()];
     ros::serialization::OStream o_stream(msg_buffer, shape_shifter.size());
+    manipulateRawData(msg_buffer);
     ros::serialization::IStream i_stream(msg_buffer, shape_shifter.size());
-    shape_shifter.write(o_stream);
-    header.seq = ((uint32_t *)msg_buffer)[0];
-    header.stamp.sec = ((uint32_t *)msg_buffer)[1];
-    header.stamp.nsec = ((uint32_t *)msg_buffer)[2];
-
-    // TODO (fkunz) check if header data is consistent
-    if ( fabs((header.stamp - ros::Time::now()).toSec()) > 5.0 )
-    {
-      ROS_WARN("Probably subscribed to a topic with a message type not "
-               "containing a header.Timestamp is: %f at Time: %f",
-               header.stamp.toSec(), ros::Time::now().toSec());
-    }
-    boost::mutex::scoped_lock config_lock(config_mutex_);
-    ROS_DEBUG("setting header stamp from %f to %f", header.stamp.toSec(),
-              (header.stamp + time_offset_).toSec());
-    header.stamp += time_offset_;
-
-    ((uint32_t *)msg_buffer)[0] = header.seq + seq_counter_++;
-    ((uint32_t *)msg_buffer)[1] = header.stamp.sec;
-    ((uint32_t *)msg_buffer)[2] = header.stamp.nsec;
     shape_shifter.read(i_stream);
 }
 
@@ -59,43 +39,46 @@ void TimeModification::startPublisherThread(const ros::NodeHandle &nh)
 
 void TimeModification::configCB(Config &config, uint32_t level)
 {
-  ROS_INFO("TimeModification configCB");
-  msg_delay_milliseconds_ = config.msg_delay_milliseconds;
-  time_offset_milliseconds_ = config.time_offset_milliseconds   ;
-  boost::mutex::scoped_lock config_lock(config_mutex_);
-  msg_delay_ = ros::Duration(msg_delay_milliseconds_/1000);
-  time_offset_ = ros::Duration(time_offset_milliseconds_/1000);
-  publish_retry_rate_ = ros::Rate(config.publish_retry_rate);
+    ROS_INFO("TimeModification configCB");
+    boost::mutex::scoped_lock config_lock(config_mutex_);
+    msg_delay_ = ros::Duration(config.msg_delay_milliseconds/1000);
+    publish_rate_ = ros::Rate(config.publish_rate);
+    time_offset_ = ros::Duration(config.time_offset_milliseconds/1000);
 }
 
 void TimeModification::inputCB(const topic_tools::ShapeShifter::ConstPtr &input)
 {
-  ROS_DEBUG("TimeModification inputCB");
-  ros::Time start_time(ros::Time::now());
-  if (!output_advertised_) {
-    ROS_INFO("Output not advertised. Setting up publisher now.");
-    ros::AdvertiseOptions opts("output", 1, input->getMD5Sum(),
-                               input->getDataType(), input->getMessageDefinition());
-    boost::mutex::scoped_lock pub_lock(pub_mutex_);
-    generic_pub_ = private_nh_.advertise(opts);
-    output_advertised_ = true;
-  }
-  uint8_t msg_buffer[input->size()];
-  ros::serialization::OStream o_stream(msg_buffer, input->size());
-  input->write(o_stream);
-  processBuffer(msg_buffer);
-//  topic_tools::ShapeShifter output;
-  StampedMsg stamped_msg(msg_buffer, start_time + msg_delay_);
-//  ros::serialization::IStream i_stream(msg_buffer, input->size());
-//  stamped_msg.first.read(i_stream);
-  boost::mutex::scoped_lock buffer_lock(buffer_mutex_);
-  stamped_msg_buffer_.push(stamped_msg);
-  ROS_DEBUG("all done");
+    ROS_DEBUG("TimeModification inputCB");
+    ros::Time start_time(ros::Time::now());
+    // Initialize.
+    if (!output_advertised_) {
+        ROS_INFO("Output not advertised. Setting up publisher now.");
+        ros::AdvertiseOptions opts("output", 1, input->getMD5Sum(),
+                                   input->getDataType(), input->getMessageDefinition());
+        boost::mutex::scoped_lock pub_lock(pub_mutex_);
+        generic_pub_ = private_nh_.advertise(opts);
+        output_advertised_ = true;
+    }
+    // Copy shape shifter data to array.
+    uint8_t msg_buffer[input->size()];
+    ros::serialization::OStream o_stream(msg_buffer, input->size());
+    input->write(o_stream);
+    // Manipulate (header) data.
+    manipulateRawData(msg_buffer);
+    // Read data from array to StampedMsg
+    boost::shared_ptr<StampedMsg> stamped_msg(new StampedMsg());
+    stamped_msg->second = start_time + msg_delay_;
+    ros::serialization::IStream i_stream(msg_buffer, input->size());
+    stamped_msg->first.read(i_stream);
+    // Push StampedMsg to Buffer for multithreaded publishing.
+    boost::mutex::scoped_lock buffer_lock(buffer_mutex_);
+    stamped_msg_buffer_.push(stamped_msg);
+    ROS_DEBUG("all done");
 }
 
-void TimeModification::processBuffer(uint8_t *msg_buffer)
+void TimeModification::manipulateRawData(uint8_t *const msg_buffer)
 {
-    ROS_DEBUG("TimeModification processShapeShifter");
+    ROS_DEBUG("TimeModification manipulateRawData");
     std_msgs::Header header;;
     header.seq = ((uint32_t *)msg_buffer)[0];
     header.stamp.sec = ((uint32_t *)msg_buffer)[1];
@@ -104,9 +87,9 @@ void TimeModification::processBuffer(uint8_t *msg_buffer)
     // TODO (fkunz) check if header data is consistent
     if ( fabs((header.stamp - ros::Time::now()).toSec()) > 5.0 )
     {
-      ROS_WARN("Probably subscribed to a topic with a message type not "
-               "containing a header.Timestamp is: %f at Time: %f",
-               header.stamp.toSec(), ros::Time::now().toSec());
+        ROS_WARN("Probably subscribed to a topic with a message type not "
+                 "containing a header.Timestamp is: %f at Time: %f",
+                 header.stamp.toSec(), ros::Time::now().toSec());
     }
     boost::mutex::scoped_lock config_lock(config_mutex_);
     ROS_DEBUG("setting header stamp from %f to %f", header.stamp.toSec(),
@@ -133,18 +116,18 @@ void TimeModification::publishMsgLoop(const ros::NodeHandle &nh)
                 continue;
             }
         }
-        publish_retry_rate_.sleep();
+        publish_rate_.sleep();
     }
 }
 
-void TimeModification::publishMsg(const StampedMsg &stamped_msg)
+void TimeModification::publishMsg(const boost::shared_ptr<StampedMsg> stamped_msg)
 {
     ROS_DEBUG("TimeModification publishMsg");
-    publishMsg(stamped_msg.first, stamped_msg.second);
+    publishMsg(stamped_msg->first, stamped_msg->second);
 }
 
-void TimeModification::publishMsg(const uint8_t &msg_buffer, const ros::Time &time_to_pub)
-    {
+void TimeModification::publishMsg(const topic_tools::ShapeShifter &msg, const ros::Time &time_to_pub)
+{
     ROS_DEBUG("TimeModification publishMsg Thread started with timestamp %f", time_to_pub.toSec());
     ros::Time end_time(ros::Time::now());
     ros::Time last_time;
@@ -159,13 +142,11 @@ void TimeModification::publishMsg(const uint8_t &msg_buffer, const ros::Time &ti
         {
             boost::mutex::scoped_lock pub_lock(pub_mutex_);
             ROS_DEBUG("publishing msg which should be held back till: %f", time_to_pub.toSec());
-            topic_tools::ShapeShifter output;
-            output.read(msg_buffer);
-            generic_pub_.publish(output);
+            generic_pub_.publish(msg);
             return;
         }
         boost::mutex::scoped_lock config_lock(config_mutex_);
-        publish_retry_rate_.sleep();
+        publish_rate_.sleep();
         end_time = ros::Time::now();
         if (initial_delay != msg_delay_)
         {
@@ -181,15 +162,15 @@ void TimeModification::publishMsg(const uint8_t &msg_buffer, const ros::Time &ti
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "time_modification_node");
-  ros::NodeHandle nh;
-  ros::Rate publish_rate(10);
+    ros::init(argc, argv, "time_modification_node");
+    ros::NodeHandle nh;
+    ros::Rate publish_rate(10);
 
-  TimeModification time_modification_node(nh, publish_rate);
-  ROS_INFO("Constructor successfull start spinning.");
-  time_modification_node.startPublisherThread(nh);
-  ROS_INFO("Publisher Thread started.");
-  ros::spin();
+    TimeModification time_modification_node(publish_rate);
+    ROS_INFO("Constructor successfull start spinning.");
+    time_modification_node.startPublisherThread(nh);
+    ROS_INFO("Publisher Thread started.");
+    ros::spin();
 
-  return 0;
+    return 0;
 }
